373064d0a19729e8375900d7e005d854cfb88682
Hackie05 <116246832+Hackie05@users.noreply.github.com>
2022-10-20 10:28:02 +0530

    Demo.java
    My first demo in GitHub

---
+++ Demo.java
@@ -0,0 +1,10 @@
+//My First Demo in GitHub
+class Demo{
+      void printGitHub(){
+           System.out.println("GitHub \nNew World");
+      }
+      public static void main(String arg[]){
+             //Functions use(Long way)
+             System.out.println(printGitHub());
+      }
+}
